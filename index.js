e087f6ea0822c667f44c305065d1c796977107f1
josephmabusela <josephmabusela5@gmail.com>
2021-11-23 22:51:13 +0200

    progress made

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 
+const AvoShopper = require('./avo-shopper');
+const pg = require('pg');
+const Pool = pg.Pool;
+//const shopper = AvoShopper(pool);
+
+
 const app = express();
 const PORT =  process.env.PORT || 3019;
 
+const connectionString = process.env.DATABASE_URL || 'postgresql://matome:pg123@localhost:5433/avo_shopper';
+
+const pool = new Pool({
+    connectionString
+});
+const avoShopper = AvoShopper(pool);
 // enable the req.body object - to allow us to use HTML forms
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,13 +28,30 @@ app.use(express.static('public'));
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-let counter = 0;
 
-app.get('/', function(req, res) {
+app.get('/', async function(req, res) {
+
+	let shops = await avoShopper.listShops();
+
 	res.render('index', {
-		counter
+		shops
 	});
 });
+
+app.post('/add', async function(req, res) {
+	console.log(req.body.deal_name);
+	let deals = await avoShopper.createDeal(req.body.deal_name)
+	res.render('add',{
+		deals
+	});
+});
+
+app.get('/deals:shop', async function(req, res) {
+
+	res.render('/deals:shop', {
+		
+	})
+})
 
 // start  the server and start listening for HTTP request on the PORT number specified...
 app.listen(PORT, function() {
